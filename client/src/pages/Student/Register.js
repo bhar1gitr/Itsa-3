24e4e971fab19a88fde1c1963723be630ea8cf9f
Bharat Sharma <21104023bharatsharma@gmail.com>
2023-10-27 22:50:23 +0530

    Update Register.js

--- client/src/pages/Student/Register.js
+++ client/src/pages/Student/Register.js
@@ -15,15 +15,28 @@ const Register = () => {
     email: '',
     password: '',
   });
+  const [emailError, setEmailError] = useState('');
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+
+    // Email validation
+    if (name === 'email' && !value.endsWith('@apsit.edu.in')) {
+      setEmailError('Email must end with @apsit.edu.in');
+    } else {
+      setEmailError('');
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
+    if (emailError) {
+      console.log('Invalid email address');
+      return;
+    }
+
     try {
       // Make a POST request to your registration endpoint on the server
       const response = await axios.post('http://localhost:4000/api/v1/student/register', formData);
@@ -42,52 +55,53 @@ const Register = () => {
       console.log('Registration failed');
     }
   };
-  
 
   return (
-    <Container sx={{margin:'70px auto'}} > 
-        <Typography variant="h5">Register</Typography>
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Name"
-                name="name"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Password"
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleInputChange}
-              />
-            </Grid>
+    <Container sx={{ margin: '70px auto' }}>
+      <Typography variant="h5">Register</Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{marginTop:'20px'}}
-          >
-            Register
-          </Button>
-        </form>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              error={!!emailError}
+              helperText={emailError}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ marginTop: '20px' }}
+        >
+          Register
+        </Button>
+      </form>
     </Container>
   );
 };
